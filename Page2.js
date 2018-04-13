f7bf06572dceed25be46eb511991cadb391ba73a
Jozef <Jozko.kelbel@gmail.com>
2018-04-12 22:29:14 -0700

    Historia

---
+++ Page2.js
@@ -0,0 +1,106 @@
+
+
+
+
+
+
+
+
+
+
+
+
+//animations
+document.getElementById("Air3_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1490px)' }
+], { 
+  // timing options
+  duration: 5000,
+  iterations: Infinity
+});
+
+document.getElementById("Air2_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1490px)' }
+], { 
+  // timing options
+  duration: 5200,
+  iterations: Infinity
+});
+
+document.getElementById("Air1_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1490px)' }
+], { 
+  // timing options
+  duration: 5300,
+  iterations: Infinity
+});
+
+document.getElementById("Soldier3_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(-1490px)' }
+], { 
+  // timing options
+  duration: 16300,
+  iterations: Infinity
+});
+document.getElementById("Soldier2_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(-1490px)' }
+], { 
+  // timing options
+  duration: 16050,
+  iterations: Infinity
+});
+document.getElementById("Soldier1_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1690px)' }
+], { 
+  // timing options
+  duration: 15000,
+  iterations: Infinity
+});
+document.getElementById("TjNzRQ_5_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1690px)' }
+], { 
+  // timing options
+  duration: 115000,
+  iterations: Infinity
+});
+document.getElementById("TjNzRQ_6_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1690px)' }
+], { 
+  // timing options
+  duration: 115000,
+  iterations: Infinity
+});
+document.getElementById("TjNzRQ_4_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(1690px)' }
+], { 
+  // timing options
+  duration: 115000,
+  iterations: Infinity
+});
+document.getElementById("Airplanes2_1_").animate([
+  // keyframes
+  { transform: 'translateX(0px)' }, 
+  { transform: 'translateX(-1690px)' }
+], { 
+  // timing options
+  duration: 15000,
+  iterations: Infinity
+});
